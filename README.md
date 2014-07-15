b0566b44d1c8c006e9e915bf5cc340d4411581f3
Casey Chow <casey.chow@gmail.com>
2014-07-15 15:26:34 -0400

    install and configure mongo adapter

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # episto
-### a Sails application
+
+## Configuration
+
+### Database
+
+For development, just fill out the correct information for `adapters_local_mongo` under `config/local.js`. In a production environment, set the `MONGO_HOST`, `MONGO_PORT`, `MONGO_USER`, `MONGO_PWD`, and `MONGO_DB` environment variables instead.
+
+## Troubleshooting
+
+* Run `$Env:GYP_MSVS_VERSION=2013` before running `npm i` on Windows if npm is returning errors that mention ` The build tools for Visual Studio 2010 (Platform Toolset = 'v100') cannot be found.`.
--- config/adapters.js
+++ config/adapters.js
@@ -12,11 +12,13 @@
  * http://sailsjs.org/#documentation
  */
 
+ var env = process.env;
+
 module.exports.adapters = {
 
   // If you leave the adapter config unspecified 
   // in a model definition, 'default' will be used.
-  'default': 'disk',
+  'default': 'mongo',
 
   // Persistent adapter for DEVELOPMENT ONLY
   // (data is preserved when the server shuts down)
@@ -24,16 +26,15 @@ module.exports.adapters = {
     module: 'sails-disk'
   },
 
-  // MySQL is the world's most popular relational database.
-  // Learn more: http://en.wikipedia.org/wiki/MySQL
-  myLocalMySQLDatabase: {
-
-    module: 'sails-mysql',
-    host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user: 'YOUR_MYSQL_USER',
-    // Psst.. You can put your password in config/local.js instead
-    // so you don't inadvertently push it up if you're using version control
-    password: 'YOUR_MYSQL_PASSWORD', 
-    database: 'YOUR_MYSQL_DB'
+  // MongoDB adapter. Schemaless NoSQL goodness.
+  // https://github.com/balderdashy/sails-mongo
+  mongo: {
+    module:   'sails-mongo',
+    host:     env.MONGO_HOST || 'localhost',
+    port:     env.MONGO_PORT ||  27017,
+    user:     env.MONGO_USER || 'episto',
+    password: env.MONGO_PWD  || 'password',
+    database: env.MONGO_DB   || 'episto'
   }
+
 };
