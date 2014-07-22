09c25e6bbf18002a6b169324573b93f665c50e6d
Casey Chow <casey.chow@gmail.com>
2014-07-22 12:09:49 -0400

    replace linker with mincer asset pipeline and bower dependency management

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,11 @@
 /**
  * app.js
  *
- * This file contains some conventional defaults for working with Socket.io + Sails.
- * It is designed to get you up and running fast, but is by no means anything special.
+ * This is the main file for common code.
  *
- * Feel free to change none, some, or ALL of this file to fit your needs!
+ *=require zepto
+ *=require socket-io
+ *=require sails.io
  */
 
 
@@ -27,7 +28,7 @@
       // server.
       ///////////////////////////////////////////////////////////
       log('New comet message received :: ', message);
-      //////////////////////////////////////////////////////
+      ////////////////////////////.//////////////////////////
 
     });
 
